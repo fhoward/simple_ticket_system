aeeec8f66ce4744a5b3ab794bcf34c78b7bb94fd
francis howard susa <francishoward28@gmail.com>
2023-12-06 17:51:26 +0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destory]
+  resources :users, only: [:new, :create, :show, :index, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
