dc643f35919692544a27c56728304ced57431603
francis howard susa <francishoward28@gmail.com>
2023-12-08 07:45:34 +0800

    added comment

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,10 +1,23 @@
 class Ticket < ApplicationRecord
     before_save :status_to_lower_case
+
     belongs_to :requester, class_name: 'User', foreign_key: 'requester_id'
+    has_many :comments, class_name: "Comment"
+
     validates :status, inclusion: { in: %w(new pending resolved) }
     validates :subject, presence: true, uniqueness: true
+    validate :must_have_comment_to_resolve, if: :resolved_status?
+
+    def resolved_status?
+        status == 'resolved'
+    end
+
+    def must_have_comment_to_resolve
+        errors.add(:base, 'A ticket must have at least one comment to be resolved') if comments.empty?
+    end
 
     def status_to_lower_case
         self.status = status.downcase if status.present?
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  resources :tickets
-  get '/sessions', to: 'sessions#new', as: 'new_sign_in'
-  post '/sign_in', to: 'sessions#create', as: 'sign_in' 
-  delete '/sign_out', to: 'sessions#destroy', as: 'sign_out'
- 
-  resources :users, only: [:new, :create, :show, :index, :update]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  resources :tickets do
+    resources :comments
+    collection do
+      post 'import'
+      get 'export'
+    end
+  end
+
+
+  get '/sessions', to: 'sessions#new', as: 'new_sign_in'
+  post '/sign_in', to: 'sessions#create', as: 'sign_in'
+  delete '/sign_out', to: 'sessions#destroy', as: 'sign_out'
+
+  resources :users, only: [:new, :create, :show, :index, :update]
   root "tickets#index"
 end
